ee719afa9a41d86aaf4fc0f2e451840b48e270a5
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-05-04 18:27:50 +0200

    Hero view UI

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,11 +3,13 @@ import {Route, Switch} from 'react-router-dom';
 import {Header} from './Header/Header';
 import {Footer} from './Footer';
 import {Content} from './Content';
+import HeroView from '../containters/Home/components/HeroView/HeroView';
 
 export const Layout = () => {
   return (
       <div>
           <Header />
+          <HeroView />
           {/* <Content /> */}
           {/* <Footer /> */}
       </div>
