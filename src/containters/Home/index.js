868de94d1b42f5e73258ed0db5db83eb601b04cf
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-05-17 00:29:59 +0200

    adding propTypes and rename files

--- src/containters/Home/index.js
+++ src/containters/Home/index.js
@@ -2,21 +2,16 @@ import React from "react";
 import { useEffect, useState } from "react";
 import './index.scss';
 import { heroView } from "./services/heroView";
-import HeroView from './components/HeroView/HeroView';
+import HeroView from './components/HeroView';
 
 export const Home = () => {
     const [heroViewData, setHeroViewData] = useState({});
 
     useEffect(() => {
-        // hero view 1.2
-        // category list 2
-        // other API 0.7
-        // TOTAL :
         Promise.all([heroView()]).then(res => {
             setHeroViewData(res[0].data);
-            // console.log('heroView', res[0].data);
         }).catch((error) => {
-            // console.log('heroView error', error);
+            console.log('heroView error', error);
         });
     }, []);
 
