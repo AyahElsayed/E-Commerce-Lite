a012bfafb456d361939b5f00325dca29adfe07be
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-05-22 13:45:05 +0200

    edit UI of arrows in the slider

--- src/containters/Home/components/CategoryList/index.js
+++ src/containters/Home/components/CategoryList/index.js
@@ -5,8 +5,32 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import PropTypes from 'prop-types';
+import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io';
 
 export const CategoryList = (props) => {
+
+    function SampleNextArrow(props) {
+        const { onClick } = props;
+        return (
+            <div className="next_arrow"
+                onClick={onClick}
+            >
+                <IoIosArrowForward />
+            </div>
+        );
+    }
+
+    function SamplePrevArrow(props) {
+        const { onClick } = props;
+        return (
+            <div
+                className="prev_arrow"
+                onClick={onClick}
+            >
+                <IoIosArrowBack/>
+            </div>
+        );
+    }
 
     const { categoryListData } = props;
     const settings = {
@@ -15,6 +39,8 @@ export const CategoryList = (props) => {
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 3,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />
     };
 
     return (
@@ -25,7 +51,7 @@ export const CategoryList = (props) => {
                         <Slider {...settings}>
                             {categoryListData.map((card, index) =>
                                 <div
-                                    className={`home_category_card  ${ (index -1) % 3 === 0 ? 'home_category_mid_card' : ''}`}
+                                    className={`home_category_card  ${(index - 1) % 3 === 0 ? 'home_category_mid_card' : ''}`}
                                     key={card.id}>
                                     <div className="home_category_content">
                                         <h3 className="home_category_title">
