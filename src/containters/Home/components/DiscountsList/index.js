0544cc703cb966d56cccc6f1d2407d95b53e2f5e
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-05-29 17:28:56 +0200

    correct implementation of Slider component

--- src/containters/Home/components/DiscountsList/index.js
+++ src/containters/Home/components/DiscountsList/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import './index.scss';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import { IoMdAperture } from 'react-icons/io';
 import { TiFlash } from 'react-icons/ti';
 import { IconContext } from 'react-icons';
@@ -10,19 +7,14 @@ import { Link } from 'react-router-dom';
 import { Line } from 'rc-progress';
 import Rate from 'rc-rate';
 import 'rc-rate/assets/index.css';
-import { NextArrow, PrevArrow } from '../../../../components/helpers/ArrowsSlider';
 import { PropTypes } from 'prop-types';
+import { SliderComponent } from '../Slider';
 
 export const DiscountsList = (props) => {
 
     const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
         slidesToShow: 4,
         slidesToScroll: 4,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
         appendDots: dots => (
             <div
                 style={{
@@ -35,6 +27,85 @@ export const DiscountsList = (props) => {
     };
 
     const { discountListData } = props;
+
+    const renderItem = (item) => {
+        return (
+            <div className="discounts_list_card">
+                <div className="discount_amount">
+                    <IconContext.Provider value={{ color: "white", className: "discount_amount_icon" }}>
+                        <div>
+                            <IoMdAperture />
+                        </div>
+                    </IconContext.Provider>
+                    save ${item.discount_amount}
+                </div>
+                <div className="discounts_list_card_content">
+                    {item.flash_deal === "true" ?
+                        <div className="flash_deal_wrapper">
+                            <div className="flash_deal">
+                                <IconContext.Provider
+                                    value={{ color: "white", className: "discount_amount_icon" }}>
+                                    <div>
+                                        <TiFlash />
+                                    </div>
+                                </IconContext.Provider>
+                                FLASH DEAL
+                            </div>
+                        </div>
+                        : ""
+                    }
+                    <div className="discount_product_image">
+                        <img src={item.image} alt="" />
+                    </div>
+                    <div className="discount_product_brand">
+                        {item.brand}
+                    </div>
+                    <div className="body_title">
+                        {item.title}
+                    </div>
+                    <div className="discount_product_rate">
+                        <Rate
+                            value={4}
+                            allowHalf="true"
+                            disabled="true"
+                        />
+                        <span className="rate_count rate_percentage">{item.rate}</span>
+                        <span className="rate_count">{item.rate_count}</span>
+                    </div>
+                    <div className="discount_product_price">
+                        ${item.price} <span className="discount_price">${item.discount_price}</span>
+                    </div>
+                    <div className="stock_count">
+                        {item.stock_count > "1" ?
+                            <div className={`progress_bar ${item.stock_count > 9 ? "greenstock" : "orangeStock"}`}>
+                                <div className="bar">
+                                    <Line
+                                        percent={item.stock_count > 20 ? "80" : "40"}
+                                        strokeWidth="10"
+                                        strokeColor={item.stock_count > 9 ? "#088a3c" : "#ffbc00"}
+                                        trailWidth="9"
+                                    />
+                                </div>
+                                <div className="bar_desc">{item.stock_count} Available in stock</div>
+                            </div>
+                            :
+                            <div className="progress_bar" style={{ color: "hotpink" }}>
+                                <div className="bar">
+                                    <Line
+                                        percent="10"
+                                        strokeWidth="10"
+                                        strokeColor="hotpink"
+                                        trailWidth="9"
+                                    />
+                                </div>
+                                <div className="bar_desc">last product in stock</div>
+                            </div>
+                        }
+                    </div>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div className="dscounts_list">
@@ -50,85 +121,12 @@ export const DiscountsList = (props) => {
                         </Link>
                     </div>
                 </div>
-
                 <div className="discounts_list_body slider_container">
-                    <Slider {...settings}>
-                        {discountListData.map((card) =>
-                            <div className="discounts_list_card">
-                                <div className="discount_amount">
-                                    <IconContext.Provider value={{ color: "white", className: "discount_amount_icon" }}>
-                                        <div>
-                                            <IoMdAperture />
-                                        </div>
-                                    </IconContext.Provider>
-                                    save ${card.discount_amount}
-                                </div>
-                                <div className="discounts_list_card_content">
-                                    {card.flash_deal === "true" ?
-                                        <div className="flash_deal_wrapper">
-                                            <div className="flash_deal">
-                                                <IconContext.Provider value={{ color: "white", className: "discount_amount_icon" }}>
-                                                    <div>
-                                                        <TiFlash />
-                                                    </div>
-                                                </IconContext.Provider>
-                                                FLASH DEAL
-                                            </div>
-                                        </div>
-                                        : ""
-                                    }
-                                    <div className="discount_product_image">
-                                        <img src={card.image} alt="" />
-                                    </div>
-                                    <div className="discount_product_brand">
-                                        {card.brand}
-                                    </div>
-                                    <div className="body_title">
-                                        {card.title}
-                                    </div>
-                                    <div className="discount_product_rate">
-                                        <Rate
-                                            value={4}
-                                            allowHalf="true"
-                                            disabled="true"
-                                        />
-                                        <span className="rate_count rate_percentage">{card.rate}</span>
-                                        <span className="rate_count">{card.rate_count}</span>
-                                    </div>
-                                    <div className="discount_product_price">
-                                        ${card.price} <span className="discount_price">${card.discount_price}</span>
-                                    </div>
-                                    <div className="stock_count">
-                                        {card.stock_count > "1" ?
-                                            <div className={`progress_bar ${card.stock_count > 9 ? "greenstock" : "orangeStock"}`}>
-                                                <div className="bar">
-                                                    <Line
-                                                        percent={card.stock_count > 20 ? "80" : "40"}
-                                                        strokeWidth="10"
-                                                        strokeColor={card.stock_count > 9 ? "#088a3c" : "#ffbc00"}
-                                                        trailWidth="9"
-                                                    />
-                                                </div>
-                                                <div className="bar_desc">{card.stock_count} Available in stock</div>
-                                            </div>
-                                            :
-                                            <div className="progress_bar" style={{ color: "hotpink" }}>
-                                                <div className="bar">
-                                                    <Line
-                                                        percent="10"
-                                                        strokeWidth="10"
-                                                        strokeColor="hotpink"
-                                                        trailWidth="9"
-                                                    />
-                                                </div>
-                                                <div className="bar_desc">last product in stock</div>
-                                            </div>
-                                        }
-                                    </div>
-                                </div>
-                            </div>
-                        )}
-                    </Slider>
+                    <SliderComponent
+                        settings={settings}
+                        discountListData={discountListData}
+                        renderItem={renderItem}
+                    />
                 </div>
             </div>
         </div>
--- src/containters/Home/components/Slider.js
+++ src/containters/Home/components/Slider.js
@@ -2,77 +2,41 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { Link } from 'react-router-dom';
-// import { NextArrow, PrevArrow } from '../../../../components/helpers/ArrowsSlider';
-// import {
-//     Title,
-//     Description,
-//     Action_link,
-//     Imag,
-//     Alt_value
-//   } from "reactstrap";
+import { NextArrow, PrevArrow } from '../../../components/helpers/ArrowsSlider';
 
 export const SliderComponent = (props) => {
-    const settings = {
+
+    const defaultSettings = {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: props.noOfShow,
-        slidesToScroll: props.noOfScroll,
-        // nextArrow: <NextArrow />,
-        // prevArrow: <PrevArrow />
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />
     };
 
-    const { categoryListData } = props;
+    let finalSettings = { ...defaultSettings };
+    const { categoryListData, settings, renderItem, discountListData } = props;
 
-    // const data = {
-    //     // props.categoryListData,
-    // }
-
-    // const KeysToComponentMap = {
-    //     title: Title,
-    //     description: Description,
-    //     action_link: Action_link,
-    //     image: Imag,
-    //     alt_value: Alt_value
-    //   };
-
-    //   const renderItem = config => {
-    //     if (typeof KeysToComponentMap[config.component] !== "undefined") {
-    //       // 
-    //     }
-    //   };
+    if (settings !== undefined) {
+        finalSettings = { ...finalSettings, ...settings }
+    }
 
     return (
         <div>
-            <Slider {...settings}>
+            <Slider {...finalSettings}>
                 {props.categoryListData ?
-                    categoryListData.map((card, index) =>
-                        <div
-                            className={`home_category_card  ${(index - 1) % 3 === 0 ? 'home_category_mid_card' : ''}`}
-                            key={card.id}>
-                            <div className="home_category_content">
-                                <h3 className="home_category_title">
-                                    {card.title}
-                                </h3>
-                                <p className="home_category_desc">
-                                    {card.description}
-                                </p>
-                                <div className="home_category_mainBtn">
-                                    <Link
-                                        to={card.action_link} >
-                                        Shop Now
-                                        </Link>
-                                </div>
-                            </div>
-                            <div className="home_category_img">
-                                <img
-                                    src={card.image}
-                                    alt={card.alt_value}
-                                />
-                            </div>
-                        </div>
-                    )
+                    categoryListData.map((item, index) => {
+                        return renderItem(item, index)
+                    })
+                    :
+                    ""
+                }
+                {props.discountListData ?
+                    discountListData.map((item) => {
+                        return renderItem(item)
+                    })
                     :
                     ""
                 }
