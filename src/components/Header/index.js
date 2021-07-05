fa2988e7a747e94f341f2c071114447482d66185
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-07-05 15:14:12 +0200

    create navbar component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,9 @@
 import './index.scss'
-import { useState } from 'react';
 import { MdLocalShipping } from 'react-icons/md'
-import { BsSearch } from 'react-icons/bs'
-import { IoIosArrowDown } from 'react-icons/io'
-import { FiShoppingCart } from 'react-icons/fi'
-
+import { NavBar } from '../NavBar';
 
 export const Header = () => {
-    const [isMenuShown, setIsMenuShown] = useState(false)
-    const [isAccountMenuShown, setIsAccountMenuShown] = useState(false)
-    const [isLanguageMenuShown, setIsLanguageMenuShown] = useState(false)
 
-    const toggleDropDown = () => {
-        setIsMenuShown(!isMenuShown)
-    }
-    const toggleAccountDropDown = () => {
-        setIsAccountMenuShown(!isAccountMenuShown)
-    }
-    const toggleLanguageDropDown = () => {
-        setIsLanguageMenuShown(!isLanguageMenuShown)
-    }
     return (
         <>
             <header className="header-top">
@@ -45,74 +29,8 @@ export const Header = () => {
                         store<span className="subLogo">logo</span>
                     </div>
 
-                    <div className="search_bar">
-                        <div className="dropdown">
-                            <button
-                                onClick={toggleDropDown}
-                                className="btn_dropdown"
-                                onBlur={() => setIsMenuShown(false)}
-                            >
-                                <div className="dropdown_des">
-                                    All products</div>
-                                <span className="icon">
-                                    <IoIosArrowDown />
-                                </span>
-                            </button>
-                            {isMenuShown &&
-                                <div className="menu">
-                                    <button className="btn_menu">Fashion</button>
-                                    <button className="btn_menu">Electronics</button>
-                                    <button className="btn_menu">Furniture</button>
-                                </div>
-                            }
-                        </div>
+                    <NavBar />
 
-                        <input placeholder="Enter keywords" />
-                        <div className="icon_search">
-                            <span><BsSearch /></span>
-                        </div>
-                    </div>
-
-                    <div className="account">
-                        <div className="dropdown">
-                            <button
-                                onClick={toggleAccountDropDown}
-                                onBlur={() => setIsAccountMenuShown(false)}
-                            >
-                                Account <span className="icon"><IoIosArrowDown /></span>
-                            </button>
-                            {isAccountMenuShown &&
-                                <div className="menu">
-                                    <button className="btn_menu">profile</button>
-                                    <button className="btn_menu">setting</button>
-                                </div>
-                            }
-                        </div>
-                    </div>
-                    <div className="line"></div>
-                    <div className="lang">
-                        <div className="flag">
-                            <div className="img"><img src="./assets/flag.png" alt="lang" /></div>
-                            <button
-                                className="icon"
-                                onClick={toggleLanguageDropDown}
-                                onBlur={() => setIsLanguageMenuShown(false)}
-                            >
-                                <span ><IoIosArrowDown /></span>
-                            </button>
-                            {isLanguageMenuShown &&
-                                <div className="menu">
-                                    <button className="btn_menu">En</button>
-                                    <button className="btn_menu">Ar</button>
-                                </div>
-                            }
-                        </div>
-                        <div className="cart">
-                            <div className="icon cart-item"><FiShoppingCart /></div>
-                            <div className="cart-item">cart</div>
-                            <div className="items cart-item">0</div>
-                        </div>
-                    </div>
                 </div>
             </header>
         </>
